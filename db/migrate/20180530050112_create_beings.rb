55a957265bc0d63de7f90c3e897483b1e9483e26
Rob <rlainewyllie@gmail.com>
2018-06-07 22:46:59 +1000

    Add attributes to the being model

--- db/migrate/20180530050112_create_beings.rb
+++ db/migrate/20180530050112_create_beings.rb
@@ -13,18 +13,25 @@ class CreateBeings < ActiveRecord::Migration[5.2]
       t.boolean :dead
 
       t.references :proficiencies, foreign_key: true
+      # skills are the individual skills such as acrobatics
       t.references :skills, foreign_key: true
+
+      # the characters survivability
       t.integer :initiative
+      t.integer :current_hp
+      t.integer :max_hp
       t.integer :level
+      t.integer :armor_class
+      # this should maybe be it's own model and I should reference it here
       t.string :hit_die
 
-      # add other attributes
+      # outline the primary main attributes
       t.integer :strength
       t.integer :dexterity
-      t.integer :
-      t.integer :
+      t.integer :constitution
       t.integer :intelligence
       t.integer :wisdom
+      t.integer :charisma
 
       # items and weapons
       t.references :backpack, foreign_key: true
