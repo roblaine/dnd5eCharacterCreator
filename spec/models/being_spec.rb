7ba11c4e51e20772cf646813667ce61f70c97f6f
Rob <rlainewyllie@gmail.com>
2018-06-28 14:55:50 +1000

    Add fixtures for beings

--- spec/models/being_spec.rb
+++ spec/models/being_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Being, type: :model do
-  
+  fixtures :beings
+  context "is an npc" do
+    it "should have an npc flag that is set to true" do
+      n = Being.new(:is_npc => true)
+      expect(n.is_npc).to eq true
+    end
+  end
+
+  context "is not an npc" do
+    it "should have an npc flag that is set to false" do
+      n = Being.new(:is_npc => false)
+      expect(n.is_npc).to eq false
+    end
+  end
 end
