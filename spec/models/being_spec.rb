6cca8368a5e6764d52dfa7854f288791c7ae8769
Rob <rlainewyllie@gmail.com>
2018-07-01 00:19:40 +1000

    Add beings fixtures for npc and non npc

--- spec/models/being_spec.rb
+++ spec/models/being_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Being, type: :model do
   fixtures :beings
+
   context "is an npc" do
     it "should have an npc flag that is set to true" do
-      n = Being.new(:is_npc => true)
+      n = beings(:npc_one)
       expect(n.is_npc).to eq true
+    end
+
+    it "should have a name of type string" do
+      n = beings(:npc_one)
+      expect(n.name.class).to eq String
     end
   end
 
   context "is not an npc" do
     it "should have an npc flag that is set to false" do
-      n = Being.new(:is_npc => false)
+      n = beings(:non_npc)
       expect(n.is_npc).to eq false
     end
   end
