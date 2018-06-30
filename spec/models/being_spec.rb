cf7ea4a6ea9ceef5b585bddb19f6c761ffad47ad
Rob <rlainewyllie@gmail.com>
2018-07-01 00:56:04 +1000

    Add test to check if being contains name column

--- spec/models/being_spec.rb
+++ spec/models/being_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe Being, type: :model do
   fixtures :beings
 
+  Being.columns_hash is_expected.to include("name")
+
   context "is an npc" do
     it "should have an npc flag that is set to true" do
       n = beings(:npc_one)
       expect(n.is_npc).to eq true
-    end
-
-    it "should have a name of type string" do
-      n = beings(:npc_one)
-      expect(n.name.class).to eq String
     end
   end
 
@@ -19,6 +16,18 @@ RSpec.describe Being, type: :model do
     it "should have an npc flag that is set to false" do
       n = beings(:non_npc)
       expect(n.is_npc).to eq false
+    end
+  end
+
+  context "either npc or non_npc" do
+    it "should have a name of type string" do
+      n = beings(:npc_one)
+      expect(n.name.class).to eq String
+    end
+
+    it "should have have a field for strength" do
+      n = beings(:npc_one)
+      # expect(n.)
     end
   end
 end
