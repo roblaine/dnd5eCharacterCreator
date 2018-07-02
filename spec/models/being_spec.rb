519e1d62a82e5de5163b46d7a8f49d7964d611f6
Rob <rlainewyllie@gmail.com>
2018-07-02 13:21:13 +1000

    Refactor unit tests to use a each do block

--- spec/models/being_spec.rb
+++ spec/models/being_spec.rb
@@ -10,10 +10,79 @@ RSpec.describe Being, type: :model do
       expect(beings(:non_npc)).to be_valid
     end
 
-    it "is not valid without a name"
-    it "is not valid without an age"
-    it "is not valid without an initiative"
-  end
+    it "is not valid without a name" do
+      b = beings(:non_npc)
+      b.name = nil
+      expect(b).to_not be_valid
+    end
+
+    it "is not valid without an age" do
+      b = beings(:non_npc)
+      b.age = nil
+      expect(b).to be_valid
+    end
+
+    it "is not valid without an initiative" do
+      b = beings(:non_npc)
+      b.initiative = nil
+      expect(b).to_not be_valid
+    end
+
+    # it "is not valid without a strength score" do
+    #   b = beings(:non_npc)
+    #   b.strength = nil
+    #   expect(b).to_not be_valid
+    # end
+
+    it "is not valid without a dexterity score" do
+      b = beings(:non_npc)
+      b.dexterity = nil
+      expect(b).to_not be_valid
+    end
+
+    it "is not valid without a wisdom score" do
+      b = beings(:non_npc)
+      b.wisdom = nil
+      expect(b).to_not be_valid
+    end
+
+    it "is not valid without an intelligence score" do
+      b = beings(:non_npc)
+      b.intelligence = nil
+      expect(b).to_not be_valid
+    end
+
+    it "is not valid without a charisma score" do
+      b = beings(:non_npc)
+      b.charisma = nil
+      expect(b).to_not be_valid
+    end
+
+    it "is not valid without a constitution score" do
+      b = beings(:non_npc)
+      b.constitution = nil
+      expect(b).to_not be_valid
+    end
+
+    fields = ['strength', 'acrobatics', 'athletics']
+    fields.each do |f|
+      it "is not valid without a #{f} score" do
+        b = beings(:non_npc)
+        b[f] = nil
+        expect(b).to_not be_valid
+      end
+    end
+
+    # it "is not valid without an acrobatics score"
+
+    # you might just be really poor :(
+    it "is valid without any gold pieces (gp)" do
+      b = beings(:non_npc)
+      b.gold_pieces = 0
+      expect(b).to be_valid
+    end
+
+  end # end of non npc
 
   context "is an npc" do
     it "is valid only with all valid attributes" do
@@ -21,8 +90,16 @@ RSpec.describe Being, type: :model do
     end
 
     # npcs may be created without any of the following and still be valid
-    it "is valid without a name"
-    it "is valid without an age"
+    it "is valid without a name" do
+      b = beings(:npc)
+      b.name = nil
+      expect(b).to be_valid
+    end
+
+    it "is valid without an age" do
+      b = beings(:npc)
+      expect(b).to be_valid
+    end
 
     it "is valid without a motivation" do
       b = beings(:npc)
@@ -30,18 +107,49 @@ RSpec.describe Being, type: :model do
       expect(b).to be_valid
     end
 
-    it "is valid without a strength score"
-    it "is valid without a dexterity score"
-    it "is valid without a constitution score"
-    it "is valid without a intelligence score"
-    it "is valid without a wisdom score"
-    it "is valid without a charisma score"
+    it "is valid without a strength score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+    it "is valid without a dexterity score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+    it "is valid without a constitution score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+    it "is valid without a intelligence score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+    it "is valid without a wisdom score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+    it "is valid without a charisma score" do
+      b = beings(:npc)
+
+      expect(b).to be_valid
+    end
+
+
+    # a goblin might just be really poor :(
+    it "is valid without any gold pieces (gp)" do
+      b = beings(:npc)
+      b.gold_pieces = 0
+      expect(b).to be_valid
+    end
   end
 
-  # you might just be really poor :(
-  it "is valid without any gold pieces (gp)" do
-    b = beings(:non_npc)
-    b.gold_pieces = 0
-    expect(b).to be_valid
-  end
 end
