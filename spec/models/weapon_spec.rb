98e14478262fe3ee3ed3b0889843ae2733beabe9
Rob <rlainewyllie@gmail.com>
2018-07-02 16:12:48 +1000

    Flesh out spec for weapons

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -2,9 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
   fixtures :weapons
-  
+
   it "is invalid without a name"
 
-  it "is invalid without "
+  it "is invalid without a superclass"
+
+  it "is valid if superclass is melee"
+
+  it "is valid if superclass is ranged"
+
+  it "is invalid without a class"
+
+  it "is invalid without a damage value"
+
+  it "is invalid without a damage type"
+
+  it "is invalid without a range"
+
+  it "is invalid without a weight"
+
+  it "is invalid without properties"
+
+  it "is invalid without silvered"
+
+  it "is invalid without a cost"
 
 end
