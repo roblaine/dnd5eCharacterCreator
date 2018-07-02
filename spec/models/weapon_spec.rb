81a85c6639c88aa4354474125dc0b17b931b2c38
Rob <rlainewyllie@gmail.com>
2018-07-02 19:04:16 +1000

    Reconsolidate ranged and melee weapons to weapons.yml
        - Class hierarchy should look as follows
        - (Ranged_weapon || Melee_weapon) < Weapon < Item

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,76 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
-  fixtures :melee_weapons, :ranged_weapons
+  fixtures :weapons
 
   it "is invalid without a name" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.name = nil
     expect(w).to_not be_valid
   end
-
-  it "is invalid without a superclass" do
-    w = melee_weapons(:axe)
-    w.superclass = nil
-    expect(w).to_not be_valid
-  end
-
-  it "is valid if superclass is melee" do
-    w = melee_weapons(:axe)
-    w.superclass = ranged_weapon
-    expect(w).to_not be_valid
-  end
-
-  it "is valid if superclass is ranged" do
-    w = ranged_weapons(:bow)
-    w.superclass = ranged_weapon
-    expect(w).to_not be_valid
-  end
-
+  
   it "is invalid without a class" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.class = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without a damage value" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.damage = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without a damage type" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.damage_type = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without a range" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.range = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without a weight" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.weight_lbs = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without properties" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.properties = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without silvered" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.silvered = nil
     expect(w).to_not be_valid
   end
 
   it "is invalid without a cost" do
-    w = melee_weapons(:axe)
+    w = weapons(:axe)
     w.cost = nil
     expect(w).to_not be_valid
   end
