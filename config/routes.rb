e66f2e9bfe36ab652a031859ef25ca87516ff8cd
Rob <rlainewyllie@gmail.com>
2018-07-04 14:38:53 +1000

    Add ammunition using STI that extends weapon

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ammunitions
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :user, only: [:new, :show, :create, :destroy]
