5b493bdc7b543dab3ca76ea0933db2b89ae34265
Rob <rlainewyllie@gmail.com>
2018-07-06 14:41:57 +1000

    Add heroku Procfile and fix being in admin view

--- app/admin/beings.rb
+++ app/admin/beings.rb
@@ -1,7 +1,12 @@
 ActiveAdmin.register Being do
   permit_params :is_npc, :name, :age, :motivation,
   :dead, :initiative, :current_hp,:max_hp, :level, :armor_class, :strength,
-  :dexterity, :constitution, :intelligence, :wisdom, :charisma, :gold_pieces
+  :dexterity, :constitution, :intelligence, :wisdom, :charisma,
+  :athletics, :acrobatics, :sleight_of_hand, :stealth, :arcana, :history,
+  :investigation, :nature, :religion, :animal_handling, :insight, :medicine,
+  :perception, :survival, :deception, :intimidation, :performance, :persuasion
+  :gold_pieces
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,7 +16,7 @@ ActiveAdmin.register Being do
 #
 # permit_params do
 # permitted = [:permitted, :attributes]
-# permitted << :other if params[:action] == 'create' && current_user.admin?
+# permitted << :other if params[:action] == create && current_user.admin?
 # permitted
 # end
 end
