26b8724adc571639c8a25e6c61bc09f4583d860a
Rob <rlainewyllie@gmail.com>
2018-07-07 19:00:17 +1000

    Fix typo in migration

--- db/migrate/20180704045135_modify_columns_on_weapons.rb
+++ db/migrate/20180704045135_modify_columns_on_weapons.rb
@@ -1,7 +1,7 @@
 class ModifyColumnsOnWeapons < ActiveRecord::Migration[5.2]
   def change
     remove_column :weapons, :primary_attack
-    remove_column :weapons, :secondary_attack
+    remove_column :weapons, :seondary_attack
     add_column :weapons, :properties, :string
     add_column :weapons, :silvered, :boolean
 
