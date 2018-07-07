3f662c7f32c8b95f1962d033c3434239fd2946cc
Rob <rlainewyllie@gmail.com>
2018-07-07 18:54:30 +1000

    Remove drop weapons table migration

--- db/migrate/20180704042335_drop_ranged_weapons_table.rb
+++
@@ -1,9 +0,0 @@
-class DropRangedWeaponsTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :ranged_weapons
-    drop_table :melee_weapons
-    drop_table :dmg_type_weapons
-    drop_table :dmg_type
-    
-  end
-end
