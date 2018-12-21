613542ebba400009971cdddb84ca798fb6680c6b
Rob Laine <rlainewyllie@gmail.com>
2018-12-22 02:18:38 +1100

    Add run scripts and app file

--- server/app.js
+++ server/app.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ 
+    extended: false 
+}));
+//app.use(cookieParser());
+
+// Declare to use the public dir 
+app.use(
+    express.static(path.join(__dirname, 'public'))
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
--- server/server.js
+++ server/server.js
@@ -16,3 +16,4 @@ app.listen(port, (err) => {
 	if(err) { console.log(err) };
 	console.log('Listening on port ' + port);
 })
+
