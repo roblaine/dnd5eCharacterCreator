256c3638a8dcc0c1489b624580207ce10c44a253
Rob Laine <rlainewyllie@gmail.com>
2018-12-31 01:34:58 +1100

    Add spell details to the spell

--- client/src/Spell.js
+++ client/src/Spell.js
@@ -10,18 +10,26 @@ class Spell extends Component {
   }
 
   componentDidMount() {
-    console.log(`\'Spell\' mounted correctly.`);
+    console.log(`'Spell' mounted correctly.`);
+    
   }
 
   render() {
     return(
       <div className="Spell">
-        <li className="Spell-details">
-        </li>
+        <ul className="Spell-details">
+          <li className="Spell-detail">Fireball</li>
+          <li className="Spell-detail">1</li>
+          <li className="Spell-detail">Evocation</li>
+          <li className="Spell-detail">Wizard</li>
+          <li className="Spell-detail">Fiery Bolt cast by a powerful mage</li>
+          <li className="Spell-detail">Non-Ritual</li>
+          <li className="Spell-detail">60 Feet</li>
+          <li className="Spell-detail">1d8</li>
+        </ul>
       </div>
     );
-  }
-  
+  } 
 }
 
 export default Spell;
