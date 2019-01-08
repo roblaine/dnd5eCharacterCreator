d3a59c90fb996e0650d2a39a4a85157c88b849c5
Rob Laine <rlainewyllie@gmail.com>
2019-01-08 11:38:02 +0100

    Add logout button to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 
 class Navbar extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  // render different links, ie logout link if user is logged in
   render() {
+    let loggedIn = this.props.auth.isAuthenticated;
+
     return (
       <div className='navbar-fixed'>
         <nav className='z-depth-10'>
@@ -17,6 +28,21 @@ class Navbar extends Component {
                 <i className='material-icons'>healing</i>
                 DND Tracker
               </Link>
+              {loggedIn ? (
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                  <button
+                    style={{
+                      width: '150px',
+                      borderRadius: '3px',
+                      letterSpacing: '1.5px',
+                      marginRight: '10px'
+                    }}
+                    onClick={this.onLogoutClick}
+                    class="waves-effect waves-light btn hoverable blue">
+                    Logout
+                  </button>
+                </ul>
+              ) : ("")}
             </div>
           </nav>
         </div>
@@ -24,4 +50,16 @@ class Navbar extends Component {
     }
   }
 
-  export default Navbar;
+  Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
+
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+
+  export default connect(
+    mapStateToProps,
+    { logoutUser }
+  )(Navbar);
