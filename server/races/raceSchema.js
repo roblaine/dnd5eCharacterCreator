2e3b55537690ceeb7bcf97a6c79d46e77346820d
Rob Laine <rlainewyllie@gmail.com>
2019-01-11 00:37:59 +0100

    Change subrace to be an array of strings

--- server/races/raceSchema.js
+++ server/races/raceSchema.js
@@ -4,40 +4,48 @@ const Schema = mongoose.Schema;
 // Base races in 5e to reference when create new character is called
 const RaceSchema = new Schema({
   name: String,
-  abilityScoreIncrease: {
-    name: String,
-    value: Number
-  },
-  size: String,
-  alignment: {
-    law: String,
-    moral: String
-  },
-  height: {
-    min: Number,
-    max: Number
-  },
-  age: {
-    lifespan: Number,
-    mature: Number,
-    mentallyMature: Number
-  },
-  subrace: [
-
+  // Subraces is just an array of race names, ie for dwarf it will be
+  // ["HillDwarf", "MountainDwarf"]
+  subraces: [
+    // Render data based on a db query against
+    // race.subraces.Map(name => (
+    //  db.races.findOne({ name: name });
+    // ));
+    String
   ],
-  speed: {
-    // eg dwarf:
-    //  Speed: 25
-    //  description: Your speed is not reduced by wearing heavy armor
-    distance: Number,
-    description: String
-  },
-  traits: [
-    {
-      name: String,
+  traits: {
+    speed: {
+      // eg dwarf:
+      //  Speed: 25
+      //  description: Your speed is not reduced by wearing heavy armor
+      distance: Number,
       description: String
-    }
-  ],
+    },
+    abilityScoreIncrease: {
+      name: String,
+      value: Number
+    },
+    size: String,
+    alignment: {
+      law: String,
+      moral: String
+    },
+    height: {
+      min: Number,
+      max: Number
+    },
+    age: {
+      lifespan: Number,
+      mature: Number,
+      mentallyMature: Number
+    },
+    generic: [
+      {
+        name: String,
+        description: String
+      }
+    ]
+  },
   modifiers: [
     {
       name: String,
