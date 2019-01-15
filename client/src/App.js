eff76e70cb19df3572b47e97a0e6d1a48d4dbb43
Rob Laine <rlainewyllie@gmail.com>
2019-01-15 14:21:07 +0100

    Add a log class and add debug with yarn

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
+import Log from "./utils/log";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { Provider } from "react-redux";
@@ -13,6 +14,11 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+
+// Check for production to disable logging
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.setItem('debug', 'dndTracker:*');
+}
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
