ba0fad5b430efead78d102bea5a5f2a0fc20c9b5
Rob Laine <rlainewyllie@gmail.com>
2019-02-14 22:45:04 +1100

    Fix comment

--- docker-push-ecr.sh
+++ docker-push-ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# push the contianers
+# push the images
 docker push 469307158432.dkr.ecr.ap-southeast-2.amazonaws.com/spacebits/dndtracker:client
 docker push 469307158432.dkr.ecr.ap-southeast-2.amazonaws.com/spacebits/dndtracker:server
