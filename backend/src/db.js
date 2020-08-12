25f9281ee7f9a099dfd3f47ed87626dff25c0f29
Rob Laine <rlainewyllie@gmail.com>
2020-08-12 14:03:42 +1000

    Change deps for prisma client

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,6 @@
 // Connects to remote Prisma db and add js querying
 const { PrismaClient } = require('@prisma/client');
+
 const db = new PrismaClient({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
